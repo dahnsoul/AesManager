10e57ce2baa1cc07d86a65681b477aa6aa61ee69
marco <dahnsoul@gmail.com>
2022-05-20 09:50:18 +0900

    1차 테스트 완료 버전

--- src/AES256.java
+++ src/AES256.java
@@ -1,3 +1,4 @@
+import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
@@ -7,14 +8,22 @@ public class AES256 {
 
 
     public static String alg = "AES/CBC/PKCS5Padding";
-    //private final String key = "C7CFD382D8D964472C602FB287FDD0E68D96447F7D0E62C77CFD382D02FB28DC";
-    //private final String iv = "4A614E645267556B4A614E645267556B";
+    private final String kbKey = "C7CFD382D8D964472C602FB287FDD0E68D96447F7D0E62C77CFD382D02FB28DC";
+    private final String kbIv = "4A614E645267556B4A614E645267556B";
 
-    private final String key = "01234567890123456789012345678901";
-    private final String iv = key.substring(0, 16);
+    //private final String kbKey = "01234567890123456789012345678901";
+    //private final String kbIv = kbKey.substring(0, 16);
 
     //암호화
     public String encrypt(String text) throws Exception {
+        /*
+        String key = hexToByteArray()
+        String iv = "";
+
+        System.out.println("before getbytes:"+ kbKey);
+        System.out.println("after getbytes:"+ kbKey.getBytes(StandardCharsets.UTF_8));
+
+
         Cipher cipher = Cipher.getInstance(alg);
         SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
         IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
@@ -22,18 +31,47 @@ public class AES256 {
 
         byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
         return Base64.getEncoder().encodeToString(encrypted);
+        */
+        byte[] key = hexToByteArray(kbKey);
+        byte[] iv = hexToByteArray(kbIv);
+
+        Cipher cipher = Cipher.getInstance(alg);
+        SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+        IvParameterSpec ivParamSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivParamSpec);
+
+        //byte[] encrypted = cipher.doFinal(text.getBytes("EUC-KR"));
+        byte[] encrypted = cipher.doFinal(text.getBytes("UTF-8"));
+        return Base64.getEncoder().encodeToString(encrypted);
+
+
     }
 
     //복호화
     public String decrypt(String cipherText) throws Exception {
-        Cipher cipher = Cipher.getInstance(alg);
-        SecretKeySpec keySpec = new SecretKeySpec(key.getBytes(), "AES");
-        IvParameterSpec ivParamSpec = new IvParameterSpec(iv.getBytes());
-        cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
 
-        byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
-        byte[] decrypted = cipher.doFinal(decodedBytes);
-        return new String(decrypted, "UTF-8");
+
+        try {
+            byte[] key = hexToByteArray(kbKey);
+            byte[] iv = hexToByteArray(kbIv);
+
+
+            Cipher cipher = Cipher.getInstance(alg);
+            SecretKeySpec keySpec = new SecretKeySpec(key, "AES");
+            IvParameterSpec ivParamSpec = new IvParameterSpec(iv);
+            cipher.init(Cipher.DECRYPT_MODE, keySpec, ivParamSpec);
+
+            byte[] decodedBytes = Base64.getDecoder().decode(cipherText);
+            byte[] decrypted = cipher.doFinal(decodedBytes);
+
+            //return new String(decrypted, "EUC-KR");
+            return new String(decrypted, "UTF-8");
+
+        } catch (Exception e){
+          System.out.println("dddd"+e);
+            throw e;
+        }
+
     }
 
     public static byte[] hexToByteArray(String hex) {
@@ -56,13 +94,32 @@ public class AES256 {
         return sb.toString();
     }
 
+
+    public  String fillWithZero(String strNVal, int nZeroCount){
+        int nLenOfVal = strNVal.length();
+        if(nLenOfVal >= nZeroCount ){
+            return strNVal;
+        } else {
+            String freeSpace ="";
+            int nSpaceLen = nZeroCount - nLenOfVal;
+
+            for(int i=0 ; i < nSpaceLen; i++) {
+
+                freeSpace += "0";
+
+            }
+            return freeSpace + strNVal;
+        }
+
+
+    }
     //테스트
     public  String test(){
 
         byte [] byteStr;
 
         //byteStr= hexToByteArray(key);
-        byteStr= hexToByteArray(iv);
+        byteStr= hexToByteArray(kbIv);
         String result = new String(byteStr);
 
         return result;
--- src/AesManagerPractice.java
+++ src/AesManagerPractice.java
@@ -5,32 +5,33 @@ public class AesManagerPractice {
     public static void main(String[] args) throws Exception {
 
         AES256 aes256 = new AES256();
+        String glDecryptLine = "";
 
         //실행 파라미터 1 : 암호화
         if (args[0].equals("1")) {
 
-            File file = new File("/Users/marco/고객정보_20220516.txt");
+            //File file = new File("/Users/marco/고객정보_20220516.txt");
+            File file = new File("/Users/marco/카드정보_20220516.txt");
 
             if (file.exists()) {
 
-
-                //BufferedReader inFile = new BufferedReader(new FileReader(file));
                 BufferedReader inFile = new BufferedReader(new InputStreamReader(new FileInputStream(file), "euc-kr"));
 
-                File outPutFile = new File("/Users/marco/고객정보_20220516_암호화.txt");
+                //File outPutFile = new File("/Users/marco/고객정보_20220516_암호화.txt");
+                File outPutFile = new File("/Users/marco/카드정보_20220516_암호화.txt");
 
                 if (!outPutFile.exists()) {
                     outPutFile.createNewFile();
                 }
 
-                System.out.println("agr" + args[0]);
+                System.out.println("암호화 프로세스 시작" + args[0]);
 
 
                 String sLine = null;
 
                 FileWriter fw = new FileWriter(outPutFile);
                 BufferedWriter writer = new BufferedWriter(fw);
-                //BufferedWriter writer = new BufferedWriter(new BufferedWriter(new OutputStreamWriter((new FileOutputStream(outPutFile)), "UTF-8")));
+
                 int line = 0;
 
                 while ((sLine = inFile.readLine()) != null) {
@@ -40,14 +41,15 @@ public class AesManagerPractice {
                         System.out.println("Header Line:" + sLine);
 
                     } else {
-                        if (sLine.startsWith("T", 0)) {
+                        String tail = "T" + aes256.fillWithZero((line -1)+"",10);
+
+                        if (sLine.startsWith(tail,0)) {
                             System.out.println("Tail Line:" + sLine);
                             writer.write(sLine);
                         } else {
                             System.out.println("Data Line:" + sLine);
                             String encLine = aes256.encrypt(sLine);
-
-
+                            System.out.println("Enc Data Size:"+encLine.length());
                             writer.write(encLine);
                         }
                     }
@@ -79,54 +81,72 @@ public class AesManagerPractice {
         //복호화
         } else {
 
-            File file = new File("/Users/marco/고객정보_20220516_암호화.txt");
+            //File file = new File("/Users/marco/고객정보_20220516_암호화.txt");
+            File file = new File("/Users/marco/카드정보_20220516_암호화.txt");
 
             if (file.exists()) {
-                //BufferedReader inFile = new BufferedReader(new FileReader(file));
+
                 BufferedReader inFile = new BufferedReader(new InputStreamReader(new FileInputStream(file), "euc-kr"));
 
-                File outPutFile = new File("/Users/marco/고객정보_20220516_복호화.txt");
+                //File outPutFile = new File("/Users/marco/고객정보_20220516_복호화.txt");
+                File outPutFile = new File("/Users/marco/카드정보_20220516_복호화.txt");
 
                 if (!outPutFile.exists()) {
                     outPutFile.createNewFile();
                 }
 
-                System.out.println("agr" + args[0]);
+                System.out.println("복호화 프로세스 시작" + "파라미터:"+args[0]);
 
 
                 String sLine = null;
 
                 FileWriter fw = new FileWriter(outPutFile);
                 BufferedWriter writer = new BufferedWriter(fw);
-                //BufferedWriter writer = new BufferedWriter(new BufferedWriter(new OutputStreamWriter((new FileOutputStream(outPutFile)), "UTF-8")));
+
                 int line = 0;
 
                 while ((sLine = inFile.readLine()) != null) {
 
                     if (line == 0) {
                         writer.write(sLine);
+                        writer.newLine();
+
                         System.out.println("Header Line:" + sLine);
 
                     } else {
-                        if (sLine.startsWith("T", 0)) {
+                        System.out.println("LINE" +line);
+                        System.out.println("LINE" + (line -1));
+                        String tail = "T" + aes256.fillWithZero((line -1)+"",9);
+
+                        if (sLine.startsWith(tail,0)) {
                             System.out.println("Tail Line:" + sLine);
                             writer.write(sLine);
+                            writer.newLine();
                         } else {
-                            System.out.println("Data Line:" + sLine);
-                            String encLine = aes256.decrypt(sLine);
-                            writer.write(encLine);
+                            String decryptLine = aes256.decrypt(sLine);
+                            glDecryptLine = decryptLine;
+
+                            if (decryptLine.startsWith("D", 0)) {
+                                System.out.println("Data Line:" + decryptLine);
+                                writer.write(decryptLine);
+                                writer.newLine();
+                                continue;
+                            }
                         }
                     }
 
-                    writer.newLine();
+
+
 
                     line++;
+
+
 
                     if (sLine.startsWith("H", 0)) {
                         continue;
                     }
 
-                    if (sLine.startsWith("D", 0)) {
+                    if (glDecryptLine.startsWith("D", 0)) {
                         continue;
                     }
 
